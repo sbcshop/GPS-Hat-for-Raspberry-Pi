8ed15f6f6d4e99de84de75a147eca02503509928
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2022-05-23 17:06:09 +0530

    Update gps_raw_data.py

--- gps_raw_data.py
+++ gps_raw_data.py
@@ -5,10 +5,10 @@ import serial
 
 
 #serial interface
-lora = serial.Serial(port='/dev/ttyS0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
+gps_module = serial.Serial(port='/dev/ttyS0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
 try:
     while 1:
-        data_Read = lora.readline()#read data comming from other pi lora Hat
+        data_Read = gps_module.readline()#read data comming from other pi lora Hat
         print(data_Read)
 
         
