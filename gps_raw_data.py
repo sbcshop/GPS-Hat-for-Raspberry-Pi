1adeadc7f3acfdb3c3030e7769cbb37eaa606304
Ted Timmons <ted@timmons.me>
2023-07-18 12:43:27 -0700

    documentation and code updates
    - make *py scripts executable (except the library)
    - reformat py with black
    - rm unnecessary GPIO import and usage
    - rm unused libs from google_map
    - use serial0 so the correct serial port is selected with pizero/pi3
    - move pdf docs to a folder
    - reformat README, add installation hints

--- gps_raw_data.py
+++ gps_raw_data.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python3
+
 # Receive Raw GPS Data
-import RPi.GPIO as GPIO
 import time
 import serial
 
 
-#serial interface
-gps_module = serial.Serial(port='/dev/ttyS0',baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
+# serial interface
+gps_module = serial.Serial(
+    port="/dev/serial0",
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1,
+)
 try:
     while 1:
-        data_Read = gps_module.readline()#read data comming from other pi lora Hat
+        data_Read = gps_module.readline()  # read data
         print(data_Read)
 
-        
+
 except KeyboardInterrupt:
-    GPIO.cleanup()
+    sys.exit(0)
